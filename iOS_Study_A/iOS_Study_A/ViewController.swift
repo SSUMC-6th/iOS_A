db2c49ded38ce2d8dd02a0ee2304894b9ebbaad5
ssumisfree <125457422+kimsoomin20221789@users.noreply.github.com>
2024-04-01 03:49:04 +0900

    [#1] 공통 프로젝트 파일 생성

---
+++ iOS_Study_A/iOS_Study_A/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Study_A
+//
+//  Created by 김수민 on 4/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
