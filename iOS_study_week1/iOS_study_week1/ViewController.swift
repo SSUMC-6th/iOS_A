ecf35373506b83d9752aa09011b58ba1ec0982cc
kuj7373 <kuj7373@naver.com>
2024-04-09 18:41:28 +0900

    [eddy/week1] 프로젝트파일 생성

---
+++ iOS_study_week1/iOS_study_week1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_study_week1
+//
+//  Created by 김의정 on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
