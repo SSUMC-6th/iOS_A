0b889320459f3bf4df4a7b55202ed49c9a606bc4
kuj7373 <kuj7373@naver.com>
2024-04-16 14:40:54 +0900

    [eddy/week3] tableview

---
+++ iOS_study_week2/iOS_study_week2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_study_week2
+//
+//  Created by 김의정 on 4/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+
+}
+
