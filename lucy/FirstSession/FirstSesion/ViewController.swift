607bf13cfa90db4641bc6a107a2e87a6b41d1c64
ssumisfree <125457422+kimsoomin20221789@users.noreply.github.com>
2024-04-16 13:27:46 +0900

    [lucy/session1] 테이블뷰 추가하기

---
+++ lucy/FirstSession/FirstSesion/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FirstSesion
+//
+//  Created by 김수민 on 4/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view = HomeView()
+    }
+}
