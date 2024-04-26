2ef15f885139cd2d1313a1ac55e47d8d77aa8662
ssumisfree <125457422+kimsoomin20221789@users.noreply.github.com>
2024-04-26 15:10:11 +0900

    [lucy/session2] 기본 파일 세팅

---
+++ lucy/SecondSession/SecondSession/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondSession
+//
+//  Created by 김수민 on 4/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
