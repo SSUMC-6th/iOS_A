74e8bd4bff55f2e84c67567da62d26b20f212932
ssumisfree <125457422+kimsoomin20221789@users.noreply.github.com>
2024-04-26 15:59:48 +0900

    [lucy/session2] closure를 사용한 MVVM

--- lucy/SecondSession/SecondSession/ViewModel/ClockViewModel.swift
+++ lucy/SecondSession/SecondSession/ViewModel/ClockViewModel.swift
@@ -5,4 +5,27 @@
 //  Created by 김수민 on 4/26/24.
 //
 
-import Foundation
+// 뷰-모델 (ViewModel)
+
+// 0. 뷰-모델 제작하기
+class ClockViewModel {
+    
+    // 클로저 변수(didSet함수) : ViewController에서의 수행 동작을 담음
+    var didChangeTime: ((ClockViewModel) -> Void)?
+    
+    
+    var closureTime: String {
+        didSet {
+            didChangeTime?(self)
+        }
+    }
+    
+    init() {
+        closureTime = Clock.currentTime()
+    }
+    
+    // 비지니스 로직
+    func checkTime() {
+        closureTime = Clock.currentTime()
+    }
+}
