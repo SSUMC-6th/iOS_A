fbb5ae51c9cef50a600845351b7fc17014aa302f
ssumisfree <125457422+kimsoomin20221789@users.noreply.github.com>
2024-04-26 15:17:28 +0900

    [lucy/session2] 기본 코드 세팅

--- lucy/SecondSession/SecondSession/Model/Clock.swift
+++ lucy/SecondSession/SecondSession/Model/Clock.swift
@@ -5,4 +5,18 @@
 //  Created by 김수민 on 4/26/24.
 //
 
-import Foundation
+import UIKit
+
+// 모델 (Model)
+class Clock {
+    static var currentTime: (() -> String) = {
+        let today = Date()
+        
+        let hours = Calendar.current.component(.hour, from: today)
+        let minutes = Calendar.current.component(.minute, from: today)
+        let minStr = String(format: "%02d", minutes)
+        let seconds = Calendar.current.component(.second, from: today)
+        let secStr = String(format: "%02d", seconds)
+        return "\(hours):\(minStr):\(secStr)"
+    }
+}
