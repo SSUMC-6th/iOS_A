4927dc437d5f0059e796660ac477fff8dd669fbe
Jiwoong CHOI <jiwoong.choi.179@icloud.com>
2024-04-28 13:42:22 +0900

    [JC/week3] 기본세팅

---
+++ JC/Week3_Mission/Week3_Mission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week3_Mission
+//
+//  Created by Jiwoong CHOI on 4/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+
+    view.backgroundColor = .systemBackground
+  }
+
+}
