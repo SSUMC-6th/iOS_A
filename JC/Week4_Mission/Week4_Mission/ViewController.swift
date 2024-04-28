c459e4e0b7c9be201d3f4d7dfc3db8bcb3d30692
Jiwoong CHOI <jiwoong.choi.179@icloud.com>
2024-04-28 13:11:58 +0900

    [JC/week4] 스토리보드 삭제 및 기본세팅

---
+++ JC/Week4_Mission/Week4_Mission/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week4_Mission
+//
+//  Created by Jiwoong CHOI on 4/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
