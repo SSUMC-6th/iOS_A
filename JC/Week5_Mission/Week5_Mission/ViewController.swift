960ddaee31048ce40b66d213a343770a31b28fb7
Jiwoong CHOI <jiwoong.choi.179@icloud.com>
2024-04-29 14:36:14 +0900

    [JC/week5] Set basic settings

---
+++ JC/Week5_Mission/Week5_Mission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week5_Mission
+//
+//  Created by Jiwoong CHOI on 4/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+
+    view.backgroundColor = .systemBackground
+  }
+
+}
