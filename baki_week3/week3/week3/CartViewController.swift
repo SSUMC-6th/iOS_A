4849273a6f45141ce7363eecddfd1f18e3d92cae
heemings <xxyachuallyx@navet.com>
2024-05-07 02:47:53 +0900

    데이터 전달

--- baki_week3/week3/week3/CartViewController.swift
+++ baki_week3/week3/week3/CartViewController.swift
@@ -5,25 +5,18 @@
 //  Created by 박희민 on 5/7/24.
 //
 
-
 import UIKit
 
 class CartViewController: UIViewController {
     
+    var quantity: Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("CartViewController - viewWillAppear()")
-        // 뷰가 나타나기 전에 필요한 작업 수행
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("CartViewController - viewDidAppear()")
-        // 뷰가 완전히 나타난 후에 필요한 작업 수행
+        view.backgroundColor = .white
+        
+        let howmanyLabel = UILabel(frame: CGRect(x: 50, y: 200, width: 200, height: 50))
+        howmanyLabel.text = "수량: \(quantity)"
+        view.addSubview(howmanyLabel)
     }
 }
--- baki_week3/week3/week3/ViewController.swift
+++ baki_week3/week3/week3/ViewController.swift
@@ -8,26 +8,95 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var selected: Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
-    }
-    
-    private func setupUI() {
+        view.backgroundColor = .white
+        
         let startButton = UIButton(type: .system)
-        startButton.setTitle("Go to Second VC", for: .normal)
+        startButton.setTitle("장바구니 담기", for: .normal)
         startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
         startButton.translatesAutoresizingMaskIntoConstraints = false
         
+        let howmanyLabel = UILabel()
+        howmanyLabel.text = "\(selected)"
+        howmanyLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let plusButton = UIButton(type: .system)
+        plusButton.setTitle("+", for: .normal)
+        plusButton.addTarget(self, action: #selector(increase), for: .touchUpInside)
+        plusButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let minusButton = UIButton(type: .system)
+        minusButton.setTitle("-", for: .normal)
+        minusButton.addTarget(self, action: #selector(decrease), for: .touchUpInside)
+        minusButton.translatesAutoresizingMaskIntoConstraints = false
+        
         view.addSubview(startButton)
+        view.addSubview(howmanyLabel)
+        view.addSubview(plusButton)
+        view.addSubview(minusButton)
+        
         NSLayoutConstraint.activate([
-            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            howmanyLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            howmanyLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            plusButton.topAnchor.constraint(equalTo: howmanyLabel.bottomAnchor, constant: 20),
+            plusButton.rightAnchor.constraint(equalTo: view.centerXAnchor, constant: -20),
+            plusButton.widthAnchor.constraint(equalToConstant: 50),
+            plusButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            minusButton.topAnchor.constraint(equalTo: howmanyLabel.bottomAnchor, constant: 20),
+            minusButton.leftAnchor.constraint(equalTo: view.centerXAnchor, constant: 20),
+            minusButton.widthAnchor.constraint(equalToConstant: 50),
+            minusButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            startButton.topAnchor.constraint(equalTo: plusButton.bottomAnchor, constant: 40),
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
+
     }
     
     @objc private func startButtonTapped() {
         let secondVC = CartViewController()
+        secondVC.quantity = selected //데이터 전달하기
         navigationController?.pushViewController(secondVC, animated: true)
+    }
+    
+    
+    //인터페이스 빌더와 소스 코드 연결... 버튼을 탭했을 때 
+    @IBAction func selecthowmanyButtonTapped(_ sender: Any) {
+        let alertController = UIAlertController(title: "수량 선택", message: nil, preferredStyle: .actionSheet)
+        
+        for i in 1...10 {
+            let action = UIAlertAction(title: "\(i)", style: .default) { _ in
+                self.selected = i
+            }
+            alertController.addAction(action)
+        }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @objc private func increase() {
+        selected += 1
+        updatehowmanyLabel()
+    }
+    
+    @objc private func decrease() {
+        if selected > 1 {
+            selected -= 1
+            updatehowmanyLabel()
+        }
+    }
+    
+    private func updatehowmanyLabel() {
+        if let howmanyLabel = view.subviews.compactMap({ $0 as? UILabel }).first {
+            howmanyLabel.text = "\(selected)"
+        }
     }
 }
