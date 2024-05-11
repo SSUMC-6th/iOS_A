feac5983381d4024381ee6cd5d2191b590187002
kuj7373 <kuj7373@naver.com>
2024-05-11 19:12:29 +0900

    코드리뷰 반영

--- iOS_study_week3/iOS_study_week3/ViewController.swift
+++ iOS_study_week3/iOS_study_week3/ViewController.swift
@@ -5,30 +5,11 @@
 //  Created by 김의정 on 5/7/24.
 //
 
-//import UIKit
-//
-//class ViewController: UIViewController {
-//    
-//    var selectedItem: String?
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        view.backgroundColor = .blue
-//        // Do any additional setup after loading the view.
-//    }
-//
-//
-//}
-
 import UIKit
 
 class ViewController: UIViewController {
-    // Property to store the selected item
-    var selectedItem: String? {
-        didSet {
-            updateLabel()
-        }
-    }
+    
+    var selectedItem: String?
     
     // Create a label to display the data
     let displayLabel: UILabel = {
@@ -42,22 +23,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white // Set a background color to see the label clearly
-        
-        setupLabel()
+        displayLabel.text = selectedItem
+        setView()
+        setConstraints()
     }
     
-    private func setupLabel() {
+    private func setView() {
         view.addSubview(displayLabel)
-        
+    }
+    
+    private func setConstraints() {
         NSLayoutConstraint.activate([
             displayLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             displayLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
-        
-        updateLabel()
     }
     
-    private func updateLabel() {
-        displayLabel.text = selectedItem
-    }
 }
