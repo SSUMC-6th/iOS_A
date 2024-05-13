c7408ae730fa83ee784234654b37cc2eb7fe62bc
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-05-14 07:18:01 +0900

    [JC/Week6] 기본세팅

---
+++ jc/Week6_Mission/Week6_Mission/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week6_Mission
+//
+//  Created by Jiwoong CHOI on 5/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
