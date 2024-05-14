fc7adb42467980f096291b9c29cb37f8774cfaf9
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-05-14 09:51:20 +0900

    [JC/Week6] 쿠폰 배너 완성

--- jc/Week6_Mission/Week6_Mission/HomeView/CouponBannerView.swift
+++ jc/Week6_Mission/Week6_Mission/HomeView/CouponBannerView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct CouponBannerView: View {
     var body: some View {
-        Text("Hello, World!")
+      ZStack {
+        RoundedRectangle(cornerRadius: 20)
+          .foregroundStyle(Color.white)
+          .frame(width: 370, height: 100, alignment: .center)
+          .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.green, lineWidth: 5))
+        
+        Text("집콕러세요? 30% 쿠폰 드려요!")
+      }
     }
 }
 
