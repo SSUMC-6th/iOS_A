8403de6d4b9c532330aa50c2fef34715579fd25a
heemings <xxyachuallyx@navet.com>
2024-05-27 00:32:23 +0900

    [baki/week7] tabView 구현

---
+++ baki/baki_week7/baki_week7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  baki_week7
+//
+//  Created by 박희민 on 5/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ baki/baki_week7/baki_week7/mainTabview.swift
@@ -0,0 +1,43 @@
+//
+//  Tabview.swift
+//  baki_week7
+//
+//  Created by 박희민 on 5/27/24.
+//
+
+import SwiftUI
+import SnapKit
+
+struct mainTabView: View {
+    
+    init() {
+                let tabBarAppearance = UITabBarAppearance()
+                tabBarAppearance.configureWithOpaqueBackground()
+                tabBarAppearance.backgroundColor = UIColor.white
+                UITabBar.appearance().standardAppearance = tabBarAppearance
+                if #available(iOS 15.0, *) {
+                    UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+                }
+            }
+    
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("홈", systemImage: "house")
+                }
+            
+            secondView()
+                .tabItem {
+                    Label("당근", systemImage: "house")
+                }
+            
+        }
+    }
+}
+
+struct TabView_Previews: PreviewProvider {
+    static var previews: some View {
+        mainTabView()
+    }
+}
