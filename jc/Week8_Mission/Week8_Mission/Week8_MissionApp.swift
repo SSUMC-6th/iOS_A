7acd8dddf397f706a5cbaff128bdbacbcbe477ee
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-06-02 10:35:13 +0900

    [JC/Week8] 기본 파일 세팅

---
+++ jc/Week8_Mission/Week8_Mission/Week8_MissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week8_MissionApp.swift
+//  Week8_Mission
+//
+//  Created by Jiwoong CHOI on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Week8_MissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
