f91dc25351d4d664225cc6f3a5f78b6e567be967
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-06-02 10:36:07 +0900

    [JC/Week9] 기본파일세팅

---
+++ jc/Week9_Mission/Week9_Mission/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week9_Mission
+//
+//  Created by Jiwoong CHOI on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jc/Week9_Mission/Week9_Mission/Week9_MissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week9_MissionApp.swift
+//  Week9_Mission
+//
+//  Created by Jiwoong CHOI on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Week9_MissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
