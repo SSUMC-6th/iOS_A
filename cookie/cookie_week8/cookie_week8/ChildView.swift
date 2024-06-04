3789c24fdf145b7044fa33701d9b9834b3c853d8
7b7hom <cherishu1012@gmail.com>
2024-06-04 15:22:52 +0900

    [cookie/week8] 금액 총합 출력

--- cookie/cookie_week8/cookie_week8/ChildView.swift
+++ cookie/cookie_week8/cookie_week8/ChildView.swift
@@ -4,24 +4,42 @@ struct ChildView: View {
     @Binding var cartSections: [CartSection]
 
     var body: some View {
-        List {
-            ForEach(cartSections, id: \.id) { section in
-                Section {
-                    ForEach(section.items, id: \.id) { item in
-                        HStack {
-                            Text(item.name)
-                            Spacer()
-                            Text("\(item.price)")
+        VStack {
+            List {
+                ForEach(cartSections, id: \.id) { section in
+                    Section {
+                        ForEach(section.items, id: \.id) { item in
+                            HStack {
+                                Text(item.name)
+                                Spacer()
+                                Text("\(item.price)")
+                            }
                         }
                     }
                 }
+                .onDelete(perform: removeItems)
             }
-            .onDelete(perform: removeItems)
+            .navigationTitle("장바구니")
+            
+            // 총 금액을 표시하는 부분
+            Text("총합 : \(totalPrice) 원")
+                .font(.title)
+                .padding()
         }
-        .navigationTitle("장바구니")
     }
 
     func removeItems(at offsets: IndexSet) {
         cartSections.remove(atOffsets: offsets)
+    }
+    
+    // 총 금액을 계산하는 함수
+    var totalPrice: Int {
+        var total: Int = 0
+        for section in cartSections {
+            for item in section.items {
+                total += item.price
+            }
+        }
+        return total
     }
 }
