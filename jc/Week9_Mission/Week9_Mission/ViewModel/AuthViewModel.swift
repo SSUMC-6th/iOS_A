04e144364c122937669b7ee37f604f14e6ccef5e
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-06-09 15:32:01 +0900

    [JC/Week9] Loosen dependency in AuthViewModel

--- jc/Week9_Mission/Week9_Mission/ViewModel/AuthViewModel.swift
+++ jc/Week9_Mission/Week9_Mission/ViewModel/AuthViewModel.swift
@@ -5,9 +5,17 @@
 //  Created by Jiwoong CHOI on 6/7/24.
 //
 
+import FirebaseAuth
 import Foundation
 
 class AuthViewModel: ObservableObject {
-  @Published var emailAddress: String = ""
-  @Published var password: String = ""
+  @Published private var currentUser: FirebaseAuth.User? = Auth.auth().currentUser
+
+  func getCurrentUserEmail() -> String {
+    if let currentUser = self.currentUser, let email = currentUser.email {
+      return email
+    } else {
+      return "NO EMAIL"
+    }
+  }
 }
