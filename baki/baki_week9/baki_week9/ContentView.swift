0ed6a95e7ecd5f83d3c3eb13f36a50f70f3166bd
heemings <xxyachuallyx@navet.com>
2024-06-19 15:17:45 +0900

    [baki/week9] 9주차 시작

---
+++ baki/baki_week9/baki_week9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  baki_week9
+//
+//  Created by 박희민 on 6/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ baki/baki_week9/baki_week9/baki_week9App.swift
@@ -0,0 +1,17 @@
+//
+//  baki_week9App.swift
+//  baki_week9
+//
+//  Created by 박희민 on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct baki_week9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
