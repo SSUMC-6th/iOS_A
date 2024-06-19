dc5143ac8157a34b4cfcb34763d45e6f709dc182
heemings <xxyachuallyx@navet.com>
2024-06-19 15:18:51 +0900

    [baki/week9] 필요한 뷰 생성

---
+++ baki/baki_week9/baki_week9/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  baki_week9
+//
+//  Created by 박희민 on 6/19/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ baki/baki_week9/baki_week9/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  baki_week9
+//
+//  Created by 박희민 on 6/19/24.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
