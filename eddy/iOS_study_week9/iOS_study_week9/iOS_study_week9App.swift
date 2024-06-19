c9865d94a5133dd6c9ce386615b497f2e5d9e7bf
kuj7373 <kuj7373@naver.com>
2024-06-19 15:20:43 +0900

    week9 로그인 구현

---
+++ eddy/iOS_study_week9/iOS_study_week9/iOS_study_week9App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_study_week9App.swift
+//  iOS_study_week9
+//
+//  Created by 김의정 on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_study_week9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
