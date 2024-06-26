6efd8fee537311498469ed9bdd0883f0b02fd0af
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-06-26 15:39:01 +0900

    [JC/Week10] 기초 Xcode 세팅 & SPM으로 패키지 추가

---
+++ jc/Week10_Mission/Week10_Mission/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week10_Mission
+//
+//  Created by Jiwoong CHOI on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jc/Week10_Mission/Week10_Mission/Week10_MissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week10_MissionApp.swift
+//  Week10_Mission
+//
+//  Created by Jiwoong CHOI on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Week10_MissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
